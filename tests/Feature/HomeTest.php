3bddd596f4097328600732fb8e864828c624bbce
David Peach <mail@davidpeach.co.uk>
2021-07-12 23:41:19 +0100

    First landing page for reboot.

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /** @test */
+    public function the_homepage_shows_my_name_and_tagline()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+        $response->assertSee('David Peach');
+        $response->assertSee('love a good story');
+    }
+}
