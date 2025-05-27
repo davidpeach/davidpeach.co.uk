ae2c9b1fe9c637c40b0391dad37874abfffa2ae5
David Peach <david.peach@getlaidbeds.co.uk>
2025-05-27 20:06:02 +0100

    New config changes.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 import path from 'path';
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { DateTime } from 'luxon';
+
 export default function(eleventyConfig) {
 
     const isProduction = process.env.NODE_ENV === 'production';
-    const mediaUrlPrefix = isProduction ? process.env.MEDIA_URL_PREFIX : '/assets';
+    // const mediaUrlPrefix = isProduction ? process.env.MEDIA_URL_PREFIX : '/assets';
+
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addFilter("readableDate", (dateObj) => {
@@ -14,13 +16,27 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("content/assets/*.{jpg,jpeg,png,gif,svg,webp,mp4,pdf}");
 
     eleventyConfig.addTransform("fix-asset-paths", async function(content) {
-        if (this.page.inputPath.startsWith('./posts/') && this.page.outputPath.endsWith('.html')) {
-            const relativePathRegex = /(href|src)="(?!\/|#|http|https)([^"]+)"/g;
-            return content.replace(relativePathRegex, (match, attribute, src) => {
-                const newPath = `${mediaUrlPrefix}/${src}`;
+
+        if (isProduction && this.page.outputPath.endsWith('.html')) {
+            const mediaUrlPrefix = process.env.MEDIA_URL_PREFIX;
+
+            // Don't run if the production URL isn't set
+            if (!mediaUrlPrefix) {
+                return content;
+            }
+
+            // Find all relative paths that start with "assets/"
+            const assetPathRegex = /(href|src)="(assets\/[^"]+)"/g;
+
+            return content.replace(assetPathRegex, (match, attribute, src) => {
+                // Prepend the full cloud URL to the existing path.
+                // `src` is already "assets/image.jpg"
+                const newPath = `${mediaUrlPrefix}/${src}`; 
                 return `${attribute}="${newPath}"`;
             });
         }
+
+        // In development, return the content without any changes
         return content;
     });
 
