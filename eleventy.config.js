b1cc77783d8c13106fd185229ccad7152c458b15
David Peach <david.peach@getlaidbeds.co.uk>
2025-05-29 11:27:15 +0100

    Add RSS plugin. Hopefully fix datetime hentry.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 // import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { DateTime } from 'luxon';
 
@@ -8,6 +9,26 @@ export default function(eleventyConfig) {
     const isProduction = process.env.NODE_ENV === 'production';
 
     eleventyConfig.addPlugin(syntaxHighlight);
+
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "atom", // or "rss", "json"
+        outputPath: "/feed.xml",
+        collection: {
+            name: "posts", // iterate over `collections.posts`
+            limit: 20,     // 0 means no limit
+        },
+        metadata: {
+            language: "en",
+            title: "David Peach's Website",
+            subtitle: "David Peach's Website",
+            base: "https://davidpeach.me/",
+            author: {
+                name: "David Peach",
+                email: "mail@davidpeach.co.uk",
+            }
+        }
+    });
+
     // eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addFilter("readableDate", (dateObj) => {
